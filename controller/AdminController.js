7c203349644b424093438c1d1a24fdc1956cf6e1
damar <muhamaddamar1812@gmail.com>
2023-10-18 14:41:14 +0700

    membuat dashboard

--- controller/AdminController.js
+++ controller/AdminController.js
@@ -7,19 +7,31 @@ const ip = require('ip');
 const ipa = ip.address();
 
 class AdminController{
-    async getAbsensi(req,res){
-        const absen = await Absen.findAll();
-        const infoUser = {
-            name: absen.name,
-            jurusan: absen.jurusan,
-            kelasan: absen.kelasan,
-            hari: absen.hari,
-            masuk: absen.masuk,
-            pulang: absen.pulang,
-            keterangan: absen.keterangan,
+    async getAbsensi(req, res) {
+        try {
+          const absen = await Absen.findAll();
+    
+          // Check if absen is an array
+          if (!Array.isArray(absen)) {
+            throw new Error('Unexpected data structure for absen');
+          }
+    
+          const infoUsers = absen.map((item) => ({
+            name: item.name,
+            jurusan: item.jurusan,
+            kelasan: item.kelasan,
+            hari: item.hari,
+            masuk: item.masuk,
+            pulang: item.pulang,
+            keterangan: item.keterangan,
+          }));
+    
+          return res.render('admin/dashboard', { data: infoUsers });
+        } catch (error) {
+          console.error(error);
+          return res.render('admin/error', { error: 'Error fetching absensi data' });
         }
-        return res.render('admin/dashboard', {infoUser});
-    }
+      }
 }
 
 module.exports = new AdminController();
